072f9eca37b75e371cb709f87359939e80b9c3ff
ScriptSmith <ScriptSmith@users.noreply.github.com>
2016-07-31 01:54:00 +1000

    testing map

--- script.js
+++ script.js
@@ -159,18 +159,20 @@ function initMap() {
 
     console.log('{"longitude": ' + map.getCenter().lng() + ',"latitude": ' + map.getCenter().lat() +',"max_distance": 100,"limit": 20}')
 
-    $.ajax({
-        url: "http://flood-risk-api.app.skyops.io/address-flood-risk/geo-search",
-        type: 'POST',
-        dataType: 'json',
-        contentType: 'application/json',
-        headers: {"x-iag-api-key": "iag-gov-hack-api"},
-        data: '{"longitude": ' + map.getCenter().lng() + ',"latitude": ' + map.getCenter().lat() +',"max_distance": 100,"limit": 20}',
-        success: function (data) {
-            alert(JSON.stringify(data));
-        },
-        error: function(){
-            alert("Cannot get data");
-        }
-    });
+    setTimeout(function(){
+        $.ajax({
+            url: "http://flood-risk-api.app.skyops.io/address-flood-risk/geo-search",
+            type: 'POST',
+            dataType: 'json',
+            contentType: 'application/json',
+            headers: {"x-iag-api-key": "iag-gov-hack-api"},
+            data: '{"longitude": ' + map.getCenter().lng() + ',"latitude": ' + map.getCenter().lat() +',"max_distance": 100,"limit": 20}',
+            success: function (data) {
+                alert(JSON.stringify(data));
+            },
+            error: function(){
+                alert("Cannot get data");
+            }
+        });
+    }, 5000)
 }
