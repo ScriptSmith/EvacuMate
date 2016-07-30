89cd9f98ed18f91bb0c73e85cd2a132f702e52e9
ScriptSmith <ScriptSmith@users.noreply.github.com>
2016-07-30 22:43:05 +1000

    testing map

--- script.js
+++ script.js
@@ -1,1 +1,147 @@
-a
+var styles =
+[
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "hue": "#FFA800"
+            },
+            {
+                "saturation": 0
+            },
+            {
+                "lightness": 0
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "hue": "#53FF00"
+            },
+            {
+                "saturation": -73
+            },
+            {
+                "lightness": 40
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "stylers": [
+            {
+                "hue": "#FBFF00"
+            },
+            {
+                "saturation": 0
+            },
+            {
+                "lightness": 0
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "hue": "#00FFFD"
+            },
+            {
+                "saturation": 0
+            },
+            {
+                "lightness": 30
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "hue": "#00BFFF"
+            },
+            {
+                "saturation": 6
+            },
+            {
+                "lightness": 8
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "hue": "#679714"
+            },
+            {
+                "saturation": 33.4
+            },
+            {
+                "lightness": -25.4
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    }
+]
+
+function initMap() {
+
+    $.getJSON("./locations.json", function(data) {
+        for (var g in data){
+            affectedArea = data[g]
+            for (var h in affectedArea["polygons"]){
+                console.log("gon")
+                polygon = affectedArea["polygons"][h]
+
+                for (var i in polygon){
+                    polygon[i]["lat"] = polygon[i]["latitude"];
+                    polygon[i]["lng"] = polygon[i]["longitude"];
+                    delete polygon[i]["latitude"]
+                    delete polygon[i]["longitude"]
+                }
+
+                var poly = new google.maps.Polygon({
+                    paths: polygon,
+                    strokeColor: '#FF0000',
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: '#FF0000',
+                    fillOpacity: 0.35
+                });
+                poly.setMap(map);
+            }
+        }
+
+
+
+    })
+
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 9,
+        center: {lat: -27.4999514, lng: 153.0154763}
+    });
+
+    map.setOptions({styles: styles});
+
+}
