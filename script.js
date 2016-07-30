13b139ace5c89eb386a874fa7a15a0bde83ed138
ScriptSmith <ScriptSmith@users.noreply.github.com>
2016-07-30 22:11:05 +1000

    testing map

--- script.js
+++ script.js
@@ -207,7 +207,18 @@ function initMap() {
 
     $.getJSON("./locations.json", function(data) {
         console.log(data)
+        var poly = new google.maps.Polygon({
+            paths: data[0]["polygons"][0],
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35
+        });
+
+        poly.setMap(map);
     })
+
 
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 3,
